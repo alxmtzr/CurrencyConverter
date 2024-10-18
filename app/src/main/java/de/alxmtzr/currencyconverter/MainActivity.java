5f766f36c50933d23a65f3ca119bd37cab4baee6
alxmtzr <ametzler188@googlemail.com>
2024-10-18 17:59:44 +0200

    adjusted layout

--- app/src/main/java/de/alxmtzr/currencyconverter/MainActivity.java
+++ app/src/main/java/de/alxmtzr/currencyconverter/MainActivity.java
@@ -1,7 +1,10 @@
 package de.alxmtzr.currencyconverter;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 
 import androidx.activity.EdgeToEdge;
@@ -11,13 +14,43 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import de.alxmtzr.currencyconverter.data.model.ExchangeRateDatabase;
+
 public class MainActivity extends AppCompatActivity {
+    // currency db
+    private ExchangeRateDatabase exchangeRateDatabase;
+
+    // spinner
+    private Spinner spinnerFromValue;
+    private Spinner spinnerToValue;
+    private List<String> currencyList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        exchangeRateDatabase = new ExchangeRateDatabase();
+        currencyList = new ArrayList<>(Arrays.asList(exchangeRateDatabase.getCurrencies()));
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, androidx.appcompat.R.layout.support_simple_spinner_dropdown_item, currencyList);
+        adapter.setDropDownViewResource(androidx.appcompat.R.layout.select_dialog_singlechoice_material);
+
+
+        // set adapter for spinner
+        spinnerFromValue = findViewById(R.id.spinner_from_value);
+        spinnerFromValue.setAdapter(adapter);
+        spinnerToValue = findViewById(R.id.spinner_to_value);
+        spinnerToValue.setAdapter(adapter);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
     }
