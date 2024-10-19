e8d6130ab47d623151d367060527b560bcfc991f
alxmtzr <ametzler188@googlemail.com>
2024-10-19 13:34:55 +0200

    added list view

--- app/src/main/java/de/alxmtzr/currencyconverter/CurrencyListActivity.java
+++ app/src/main/java/de/alxmtzr/currencyconverter/CurrencyListActivity.java
@@ -1,6 +1,8 @@
 package de.alxmtzr.currencyconverter;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +16,18 @@ public class CurrencyListActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_currency_list);
+
+        String[] months = { "January", "February", "March", "April",
+                "May", "June", "July", "August",
+                "September", "October", "November", "December"};
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                R.layout.list_view_item,
+                R.id.list_text_view_item,
+                months);
+
+        ListView listView = (ListView) findViewById(R.id.currency_list_view);
+        listView.setAdapter(adapter);
 
     }
 }
